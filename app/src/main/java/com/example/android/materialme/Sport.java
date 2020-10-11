3424a4b1160ba2ba640d2bebc463e26714a064cc
CrispyCabot <18431358+CrispyCabot@users.noreply.github.com>
2020-10-11 17:39:52 -0400

    completed assignment

--- app/src/main/java/com/example/android/materialme/Sport.java
+++ app/src/main/java/com/example/android/materialme/Sport.java
@@ -25,15 +25,22 @@ class Sport {
     private String title;
     private String info;
 
+    private final int imageResource;
+
     /**
      * Constructor for the Sport data model.
      *
      * @param title The name if the sport.
      * @param info Information about the sport.
      */
-    Sport(String title, String info) {
+    Sport(String title, String info, int imageResource) {
         this.title = title;
         this.info = info;
+        this.imageResource = imageResource;
+    }
+
+    public int getImageResource() {
+        return imageResource;
     }
 
     /**
